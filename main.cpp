cd5ebede9ec066669cb6bb4e07557cdd416336ef
Eoloc <eoloc57@gmail.com>
2017-02-24 16:09:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+   ifstream fluxReadme("Readme.txt".c_str());
+   string textReadme;
+   
+   while(getline(fluxReadme, textReadme))
+   {
+      cout << textReadme << endl;
+   }
    
    return 0;
 }
